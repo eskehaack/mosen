3d97942594a251fd887bf4901cbee8a39c5c85f1
Eske Haack <eskehaack@gmail.com>
2024-02-24 15:49:59 +0100

    added general table definition with scroling

--- src/components.py
+++ src/components.py
@@ -1,10 +1,23 @@
-from dash import dcc, html
+from dash import dcc, html, dash_table
 
 
 def get_upload(id: str):
     return dcc.Upload(
         ["Drag and Drop or ", html.A("Select a File")],
         id={"index": id, "type": "database_upload"},
+    )
+
+
+def get_table(id, data):
+    return dash_table.DataTable(
+        id=id,
+        data=data,
+        row_deletable=False,
+        fixed_rows={"headers": True},
+        style_table={
+            "height": "300px",
+            "overflowY": "auto",
+        },
     )
 
 
